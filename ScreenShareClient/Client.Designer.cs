867748e317ab351f3756d75a2457f7d7d8715655
RHermanet14 <rhermanet14@gmail.com>
2025-10-27 12:06:47 -0400

    Fix: Size of image on picturebox now fits correctly

--- ScreenShareClient/Client.Designer.cs
+++ ScreenShareClient/Client.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // PreferencesButton
             // 
-            PreferencesButton.Location = new Point(366, 415);
+            PreferencesButton.Location = new Point(225, 415);
             PreferencesButton.Name = "PreferencesButton";
             PreferencesButton.Size = new Size(134, 23);
             PreferencesButton.TabIndex = 1;
@@ -52,7 +52,7 @@
             // 
             // ConnectButton
             // 
-            ConnectButton.Location = new Point(628, 414);
+            ConnectButton.Location = new Point(492, 415);
             ConnectButton.Name = "ConnectButton";
             ConnectButton.Size = new Size(77, 23);
             ConnectButton.TabIndex = 2;
@@ -62,7 +62,7 @@
             // 
             // DisconnectButton
             // 
-            DisconnectButton.Location = new Point(711, 414);
+            DisconnectButton.Location = new Point(575, 415);
             DisconnectButton.Name = "DisconnectButton";
             DisconnectButton.Size = new Size(77, 23);
             DisconnectButton.TabIndex = 3;
@@ -73,7 +73,7 @@
             // IPLabel
             // 
             IPLabel.AutoSize = true;
-            IPLabel.Location = new Point(12, 419);
+            IPLabel.Location = new Point(12, 385);
             IPLabel.Name = "IPLabel";
             IPLabel.Size = new Size(77, 15);
             IPLabel.TabIndex = 4;
@@ -81,7 +81,7 @@
             // 
             // IPTextBox
             // 
-            IPTextBox.Location = new Point(92, 415);
+            IPTextBox.Location = new Point(95, 382);
             IPTextBox.Name = "IPTextBox";
             IPTextBox.Size = new Size(124, 23);
             IPTextBox.TabIndex = 5;
@@ -90,7 +90,7 @@
             // PortLabel
             // 
             PortLabel.AutoSize = true;
-            PortLabel.Location = new Point(233, 419);
+            PortLabel.Location = new Point(12, 415);
             PortLabel.Name = "PortLabel";
             PortLabel.Size = new Size(32, 15);
             PortLabel.TabIndex = 6;
@@ -98,16 +98,16 @@
             // 
             // PortTextBox
             // 
-            PortTextBox.Location = new Point(271, 415);
+            PortTextBox.Location = new Point(95, 412);
             PortTextBox.Name = "PortTextBox";
-            PortTextBox.Size = new Size(73, 23);
+            PortTextBox.Size = new Size(124, 23);
             PortTextBox.TabIndex = 7;
             PortTextBox.KeyPress += PortTextBox_KeyPress;
             // 
             // FullscreenButton
             // 
             FullscreenButton.Font = new Font("Arial Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            FullscreenButton.Location = new Point(769, 375);
+            FullscreenButton.Location = new Point(633, 378);
             FullscreenButton.Name = "FullscreenButton";
             FullscreenButton.Size = new Size(19, 22);
             FullscreenButton.TabIndex = 8;
@@ -121,7 +121,7 @@
             pictureBox.BorderStyle = BorderStyle.Fixed3D;
             pictureBox.Location = new Point(12, 12);
             pictureBox.Name = "pictureBox";
-            pictureBox.Size = new Size(776, 357);
+            pictureBox.Size = new Size(640, 360);
             pictureBox.TabIndex = 9;
             pictureBox.TabStop = false;
             // 
@@ -129,7 +129,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(660, 450);
             Controls.Add(pictureBox);
             Controls.Add(FullscreenButton);
             Controls.Add(PortTextBox);
